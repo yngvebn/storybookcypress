6711b70c0e37378db052d5f37f3b8f87abd0d470
Yngve Bakken-Nilsen <yngve.nilsen@gmail.com>
2021-02-04 20:46:19 +0100

    add failing test

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -18,7 +18,7 @@ const Template: Story<Button> = (args: Button) => ({
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Click me!',
 };
 
 export const Secondary = Template.bind({});
