da1e96790aeddd7923b84cad09a60b81f426b200
Yngve Bakken-Nilsen <yngve.nilsen@gmail.com>
2021-02-05 08:48:07 +0100

    remove comments

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -5,10 +5,8 @@ context('Actions', () => {
     cy.visit('/iframe.html?id=example-button--primary&viewMode=story')
   })
 
-  // https://on.cypress.io/interacting-with-elements
 
   it('Find button', () => {
-    // https://on.cypress.io/type
     cy.get('.storybook-button')
       .should('contain.text', 'Button')
 
